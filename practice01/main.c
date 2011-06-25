e8c9970304117cb55ad5bbceffceff260c719dca
Masato Fujii <masato@masato-xMac.local>
2011-06-25 19:36:38 +0900

    Initial Commit

---
+++ practice01/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  practice01
+//
+//  Created by Masato Fujii on 11/06/25.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
